b392ebe539927ef22da81d7789b1b16f7ec2b8af
Stefan Valuta <gheorghe_stefan_stelian@yahoo.com>
2023-10-01 20:20:35 +0300

    first project

---
+++ ProjectFlowPro.Api/Program.cs
@@ -0,0 +1,56 @@
+
+using System.IO.Pipes;
+
+namespace ProjectFlowPro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            DotNetEnv.Env.Load("dev.env");
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            ServiceInjections(builder);
+            RepositoryInjections(builder);
+            UtilityInjections(builder);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+
+        private static void ServiceInjections(WebApplicationBuilder builder)
+        {
+
+        }
+
+        private static void RepositoryInjections(WebApplicationBuilder builder)
+        {
+
+        }
+
+        private static void UtilityInjections(WebApplicationBuilder builder)
+        {
+
+        }
+    }
+}
